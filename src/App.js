035c5ff64bc0fc6367b5e1a77675c9b44bb101c8
Angel-410 <angel.arriaga.2008@gmail.com>
2024-10-19 17:30:14 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// Importación de paquetes
+import React from 'react';
+import './App.css';
+
+// Agregamos los componentes que diseñamos previamente
+import ContentList from './componentes/contentList';
+import AddContent from './componentes/addContent';
+
+function App() {
+// En nuestro sector JSX, agregamos los componentes que importamos.
+return (
+<div className="App">
+<h1>Mi primer (o segunda/tecera) app de MERN</h1>
+<AddContent />
+<ContentList />
+</div>
+);
+}
+
+export default App;
