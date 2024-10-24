260941a945123f088eb5843ea113c3f515dfb43a
Angel AR <angel.arriaga.2008@gmail.com>
2024-10-24 14:14:03 -0700

    modificacion dummy App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 // En nuestro sector JSX, agregamos los componentes que importamos.
 return (
 <div className="App">
-<h1>Mi primera app de MERN</h1>
+<h1>Mi primera app de MERN que consume una API</h1>
 <AddContent />
 <ContentList />
 </div>
