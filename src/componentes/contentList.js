a54120ecc1a47b7fd76129116d89b79da8548548
Angel AR <angel.arriaga.2008@gmail.com>
2024-10-24 14:34:44 -0700

    Reemplazo contenido por pelicula en linea 27 de contentList.js

--- src/componentes/contentList.js
+++ src/componentes/contentList.js
@@ -24,7 +24,7 @@ return (
 <h1>Los contenidos son los siguientes</h1>
 <ul>
 {contents.map((content) => (
-<li key={content._id}>{content.name} (Content: {content.content})</li>
+<li key={content._id}>{content.name} (Hoy puedes ver la siguiente pelicula: {pelicula.content})</li>
 ))}
 </ul>
 </div>
