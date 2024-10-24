34a26c0f7ae2f17d25ec0015a64e427c30ea62a6
Angel AR <angel.arriaga.2008@gmail.com>
2024-10-23 20:20:32 -0700

    Quité mi segunda o tercera app en App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 // En nuestro sector JSX, agregamos los componentes que importamos.
 return (
 <div className="App">
-<h1>Mi primer (o segunda/tecera) app de MERN</h1>
+<h1>Mi primer app de MERN</h1>
 <AddContent />
 <ContentList />
 </div>
